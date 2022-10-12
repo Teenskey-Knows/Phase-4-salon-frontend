9ac7e12b62a3bc4e86d6575096d56ef8b981d31a
sobitd <sobitdaniel@gmail.com>
2022-10-12 18:04:08 +0300

    Create Navbar component and add to App.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,1 +1,19 @@
 import React from "react";
+import "./Navbar.css";
+
+function Header() {
+  return (
+    <header className="container">
+      <nav className="nav">
+        <ul className="nav_items nav-items-flex">
+          <li className="nav_item">Suka</li>
+          <li className="nav_item">SERVICES</li>
+          <li className="nav_item">Log In </li>
+          <li className="nav_item">Sign Up</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;

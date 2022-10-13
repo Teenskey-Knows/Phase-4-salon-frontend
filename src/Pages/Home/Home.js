59b3cf189d8582878323ab20a7814cdcd0a760b8
sobitd <sobitdaniel@gmail.com>
2022-10-13 16:30:39 +0300

    create pages for Saloon web app

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,8 @@
 import React from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
 import "./Home.css";
+
+function Home() {
+  return <h1> Homepage</h1>;
+}
+
+export default Home;

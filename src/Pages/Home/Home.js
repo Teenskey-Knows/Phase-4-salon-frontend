200fb9f94176888ac2517461f1161a4d89e167b0
sobitd <sobitdaniel@gmail.com>
2022-10-13 23:39:54 +0300

    updates on home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Home.css";
 
+
 function Home() {
-  return <h1> Homepage</h1>;
+  return (
+    <div className="container-wrapper">
+    <div className="text-container">
+      <h3>Welcome to the Home of Beauty!</h3>
+    </div>
+    <div className="image-container">
+      <img src="https://media.istockphoto.com/photos/photo-of-young-curly-girl-picture-id1285132706"/>
+    </div>
+    </div>
+  )
 }
 
 export default Home;

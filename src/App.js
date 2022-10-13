d73e90c89543120cb23461331fde50bb1670bf59
sobitd <sobitdaniel@gmail.com>
2022-10-13 17:00:22 +0300

    update routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-
+import { ReactDOM } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout /Layout";
+import Home from "../src/Pages/Home/Home";
+import Services from "../src/Pages/Services/Services";
+import About from "../src/Pages/About /About";
+import Signup from "../src/Pages/Signup/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="services" element={<Services />} />
+          <Route path="about" element={<About />} />
+          <Route path="signup" element={<Signup />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+ReactDOM.render(<App />, document.getElementById("root"));

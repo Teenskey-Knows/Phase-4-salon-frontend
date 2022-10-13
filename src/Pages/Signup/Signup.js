11dd8d2aa3b3d9993f5b8afe0c8e15e2c1e365ca
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-10-13 21:30:42 +0300

    modified Signup.js

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -3,9 +3,9 @@ import './Signup.css'
 
 function Signup(){
     return (
-        
+<div>
         <form action="action_page.php" style="border:1px solid #ccc">
-  <div class="container">
+  
     <h1>Sign Up</h1>
     <p>Please fill in this form to create an account.</p>
     
@@ -14,10 +14,10 @@ function Signup(){
     <input type="text" placeholder="Enter Email" name="email" required></input>
 
     <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+    <input type="password" placeholder="Enter Password" name="psw" required></input>
 
     <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" required></input>
 
     <label>
       <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me</input>
@@ -28,8 +28,9 @@ function Signup(){
     
       <button type="button" class="cancelbtn">Cancel</button>
       <button type="submit" class="signupbtn">Sign Up</button>
-</div>
+
 </form>
+</div>
     )
     
 }

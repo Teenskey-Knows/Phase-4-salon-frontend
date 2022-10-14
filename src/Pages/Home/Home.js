cb1eecb4780e0c734c2751f97ef1840c2e3905e2
sobitd <sobitdaniel@gmail.com>
2022-10-14 08:40:28 +0300

    styling updates for homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,10 @@
+import { Card } from "@mui/material";
 import React from "react";
 import "./Home.css";
 
 function Home() {
   return (
+    <div className="container">
     <div className="container-wrapper">
       <div className="text-container">
         <h3>Care of <br/> your body</h3>
@@ -10,6 +12,12 @@ function Home() {
       <div className="image-container">
         <img src="https://as2.ftcdn.net/v2/jpg/01/74/54/43/1000_F_174544330_zNsy0mU3ix2noU0cKKHirBQJQsvmPkOE.jpg" />
       </div>
+    </div>
+    <br/>
+    <br/>
+    <hr/>
+    <br/>
+    <br/>
     </div>
   );
 }

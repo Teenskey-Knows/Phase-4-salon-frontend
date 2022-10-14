3ffdb3738905d8e5337da766945170f36dd1a804
sobitd <sobitdaniel@gmail.com>
2022-10-14 07:19:11 +0300

    modify homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,18 +1,17 @@
 import React from "react";
 import "./Home.css";
 
-
 function Home() {
   return (
     <div className="container-wrapper">
-    <div className="text-container">
-      <h3>Welcome to the Home of Beauty!</h3>
+      <div className="text-container">
+        <h3>Care of <br/> your body</h3>
+      </div>
+      <div className="image-container">
+        <img src="https://as2.ftcdn.net/v2/jpg/01/74/54/43/1000_F_174544330_zNsy0mU3ix2noU0cKKHirBQJQsvmPkOE.jpg" />
+      </div>
     </div>
-    <div className="image-container">
-      <img src="https://media.istockphoto.com/photos/photo-of-young-curly-girl-picture-id1285132706"/>
-    </div>
-    </div>
-  )
+  );
 }
 
 export default Home;

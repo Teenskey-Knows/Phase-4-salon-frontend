16536facb3b0da5d2455e206ae8eff4e7bc9d247
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-10-16 11:02:59 +0300

    modifiied index.css

--- src/App.js
+++ src/App.js
@@ -6,8 +6,20 @@ import Services from "../src/Pages/Services/Services";
 import About from "../src/Pages/About /About";
 import Signup from "../src/Pages/Signup/Signup";
 import Login from "../src/Pages/Login/Login";
-
+import { useState,useEffect } from "react";
 export default function App() {
+
+  const [provider, setProvider] = useState(null);
+
+
+  useEffect(() => {
+    // auto-login
+    fetch("https://thawing-journey-77356.herokuapp.com/provider/me").then((r) => {
+      if (r.ok) {
+        r.json().then((provider) => setProvider(provider));
+      }
+    });
+  },[]);
   return (
     <>
       <Router>
@@ -17,6 +29,7 @@ export default function App() {
           <Route path="/services" element={<Services />} />
           <Route path="/about" element={<About />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login setProvider={setProvider}/>} />
         </Routes>
       </Router>
     </>

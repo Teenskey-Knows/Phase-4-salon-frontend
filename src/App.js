674952b3b9fdc7d951ecce21a6fc3361e8e38ea7
sobitd <sobitdaniel@gmail.com>
2022-10-16 06:30:44 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
+
 import { ReactDOM } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout /Layout";
-import Home from "../src/Pages/Home/Home";
-import Services from "../src/Pages/Services/Services";
-import About from "../src/Pages/About /About";
-import Signup from "../src/Pages/Signup/Signup";
+import Services from "./Pages/Services/Services";
+import About from "./Pages/About /About";
+import Signup from "./Pages/Signup/Signup";
+import Home from "./Pages/Home/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
-    // <BrowserRouter>
-    <div>
-      <Home />
-      <Services />
-      <About />
-
-    </div>
-  //     {/* <Routes>
-  //       <Route path="/" element={<Layout />}>
-  //         <Route index element={<Home />} />
-  //         <Route path="services" element={<Services />} />
-  //         <Route path="about" element={<About />} />
-  //         <Route path="signup" element={<Signup />} />
-  //       </Route>
-  //     </Routes>
-  //   </BrowserRouter> */}
+    <>
+      <Router>
+        <Layout />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </>
   );
-// {/* } */
 }
-
 
